b697d998b605cbce9da2aa129748e8b17cc03684
shudhodhan1089 <shudhodhanj2004@gmail.com>
2025-09-22 17:08:13 +0530

    These are something for Experiment-01 for uploading

---
+++ labExperiment.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cmath>
+#include<vector>
+#include<ctime>
+#include<cstdlib>
+using namespace std;
+int main(){
+    int n=100;
+    int course_Code[100];
+    srand(time(0));
+    cout<<"Randomly generated course codes:\n";
+    for(int i=0;i<n;i++){
+        course_Code[i]=rand()%100+100;//course codes between 100-199
+        cout<<course_Code[i]<<" , ";
+    }
+    cout<<endl;
+    long long totalInversions=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(course_Code[i]>course_Code[j]){
+                totalInversions++;
+            }
+        }
+    }
+    cout<<"\nTotal Inversions (Brute Force) = "<<totalInversions<<endl;
+
+    return 0; 
+}
