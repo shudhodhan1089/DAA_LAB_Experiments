c3ad2c0c80b1c5614a67d61a59ab4a105e7289c2
shudhodhan1089 <shudhodhanj2004@gmail.com>
2025-09-29 14:14:00 +0530

    Some misunderstandings in the following code

--- labExperiment.cpp
+++ labExperiment.cpp
@@ -3,15 +3,23 @@
 #include<vector>
 #include<ctime>
 #include<cstdlib>
+#include<map>
 using namespace std;
 int main(){
     int n=100;
     int course_Code[100];
     srand(time(0));
     cout<<"Randomly generated course codes:\n";
+    int num=1;
+    map<int, vector<int>> invGroups;
+    while (num<=100)
+    {
+        /* code */
+    
+    cout<<"Choices of Student :-"<<num<<endl;
     for(int i=0;i<n;i++){
         course_Code[i]=rand()%100+100;//course codes between 100-199
-        cout<<course_Code[i]<<" , qw";
+        cout<<course_Code[i]<<" ";
     }
     cout<<endl;
     long long totalInversions=0;
@@ -19,10 +27,24 @@ int main(){
         for(int j=0;j<n;j++){
             if(course_Code[i]>course_Code[j]){
                 totalInversions++;
+                if(totalInversions<=3){
+                    invGroups[totalInversions].push_back(num);
+                }
             }
         }
     }
-    cout<<"\nTotal Inversions (Brute Force) = "<<totalInversions<<endl;
-
+    cout<<"\nTotal Inversions (Brute Force) for Student "<<num<<" "<<totalInversions<<endl;
+    cout<<endl;
+    num++;
+}
+cout<<"Students with 0,1,2,3 inversions"<<endl;
+for(int i=0;i<3;i++){
+    cout<<"Students with "<< i << "inversions :- ";
+    if(invGroups.count(i)){
+        for(int id:invGroups[i]) cout << id<<" ";
+        cout<<endl;
+    }
+    else cout<<"None"<<endl;
+}
     return 0; 
 }
